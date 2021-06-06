8d9ca2df7685f87ba06dab9cf78d461d5a1191b5
Yujin Jeong <whitepigeon77@gmail.com>
2021-06-06 16:10:35 +0900

    update allow host

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '54.180.81.21']
+ALLOWED_HOSTS = ['localhost', '54.180.81.21', '*.amazonaws.com', 'amazonaws.com', 'saeromsuh.jaeyongpark.net' ]
 
 
 # Application definition
