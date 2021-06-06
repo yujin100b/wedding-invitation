f11d79e59eda22943b913238fff5510941fcd470
Yujin Jeong <whitepigeon77@gmail.com>
2021-06-06 16:12:00 +0900

    allow all host

--- config/settings.py
+++ config/settings.py
@@ -57,7 +57,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = [
     "http://localhost:8080",
